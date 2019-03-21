b16b50f161d9cde3bfabd982e37a10af7a494a65
KaoutarOULAHYANE <kaoutar.oulahyane@gmail.com>
2019-03-21 16:44:57 +0100

    commit

---
+++ users.js
@@ -0,0 +1,25 @@
+var users = [
+    {
+        picture:'https://lorempixel.com/300/300/people?123',
+        name:'OULAHYANE Kaoutar',
+        bio:'Eleve ingenieur'
+    },
+
+    {
+        picture:'https://lorempixel.com/300/300/people?800',
+        name:'OUFTOU Mohamed',
+        bio:'Eleve ingenieur'
+    },
+
+    {
+        picture:'https://lorempixel.com/300/300/people?54',
+        name:'OULAHYANE Zaina',
+        bio:'Eleve ingenieur'
+    },
+
+    {
+        picture:'https://lorempixel.com/300/300/people?10',
+        name:'OUFTOU Nada',
+        bio:'Eleve ingenieur'
+    },
+]
